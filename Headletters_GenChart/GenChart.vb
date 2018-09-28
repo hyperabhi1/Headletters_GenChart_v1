2e1d91e1475d4a316e1d2ad65a43ed0840f9d931
Kirti Vashishtha <kirti.vashishtha@enukesoftware.com>
2018-09-28 11:31:22 +0530

    Initials

---
+++ Headletters_GenChart/GenChart.vb
@@ -0,0 +1,340 @@
+﻿Imports System.Collections.Specialized
+Imports System.Data.SqlClient
+Imports System.Globalization
+Imports System.IO
+
+Public Class GenChart
+    Public Shared Sub Main()
+        Dim UID = ""
+        Dim HID = ""
+        Dim DateTimeB As New NameValueCollection
+        Dim PlaceDataB As New NameValueCollection
+        Dim BData As New NameValueCollection
+        Dim PlaceData As New NameValueCollection
+        Dim personalDetails As PersonalDetails = New PersonalDetails()
+        Dim P_list(12) As String
+        Dim H_List(12) As String
+        Dim HstrCusp(12) As String
+        Dim Hplanets(12) As String
+        Dim BirthLagna(12, 2) As String
+        Dim BirthBhav(12, 2) As String
+        Dim BirthSouth(12) As String
+        Dim Horo As New TASystem.TrueAstro
+
+        Dim connection As SqlConnection = New SqlConnection("data source=49.50.103.132;initial catalog=ASTROLOGYSOFTWARE_DB;integrated security=False;User Id=sa;password=pSI)TA1t0K[)")
+        Try
+            Dim cmd As New SqlCommand($"SELECT DISTINCT HUSERID, HID, RECTIFIEDDATE, RECTIFIEDTIME, RECTIFIEDDST, RECTIFIEDPLACE, 
+                                    RECTIFIEDLONGTITUDE, RECTIFIEDLONGTITUDEEW, RECTIFIEDLATITUDE,RECTIFIEDLATITUDENS, RECTIFIEDTZ
+                                    FROM HMAIN
+                                    INNER JOIN HREQUEST
+                                    ON HMAIN.HUSERID = HREQUEST.RQUSERID
+                                        AND HMAIN.HID = HREQUEST.RQHID
+                                    WHERE HREQUEST.REQCAT = '9'
+                                        AND HREQUEST.RQUNREAD = 'Y';", connection)
+            Dim da As New SqlDataAdapter(cmd)
+            Dim RowsData As New DataSet()
+            da.Fill(RowsData)
+            For i As Integer = 0 To RowsData.Tables(0).Rows.Count - 1
+                Try
+                    UID = RowsData.Tables(0).Rows(i)(0).Trim.ToString()
+                    HID = RowsData.Tables(0).Rows(i)(1).Trim.ToString()
+                    Dim RECTIFIEDDATE = CType(RowsData.Tables(0).Rows(i)(2), DateTime).ToString("yyyy-MM-dd HH:mm:ss.fff", CultureInfo.InvariantCulture)
+                    Dim RECTIFIEDTIME = CType(RowsData.Tables(0).Rows(i)(3).ToString(), DateTime).ToString("HH:mm:ss.fff", CultureInfo.InvariantCulture)
+                    Dim RECTIFIEDDST = RowsData.Tables(0).Rows(i)(4).ToString().Trim
+                    Dim RECTIFIEDPLACE = RowsData.Tables(0).Rows(i)(5).ToString().Trim
+                    Dim RECTIFIEDLONGTITUDE = RowsData.Tables(0).Rows(i)(6).Trim.ToString().Trim
+                    Dim RECTIFIEDLONGTITUDEEW = RowsData.Tables(0).Rows(i)(7).ToString().Trim
+                    Dim RECTIFIEDLATITUDE = RowsData.Tables(0).Rows(i)(8).ToString().Trim
+                    Dim RECTIFIEDLATITUDENS = RowsData.Tables(0).Rows(i)(9).ToString().Trim
+                    Dim RECTIFIEDTZ = RowsData.Tables(0).Rows(i)(10).ToString().Trim
+                    Dim Year = RECTIFIEDDATE.Split("-")(0)
+                    Dim Month = RECTIFIEDDATE.Split("-")(1)
+                    Dim Day = RECTIFIEDDATE.Split("-")(2).Substring(0, 2)
+                    Dim Hour = RECTIFIEDTIME.Split(":")(0)
+                    Dim Min = RECTIFIEDTIME.Split(":")(1)
+                    Dim Sec = RECTIFIEDTIME.Split(":")(2).Substring(0, 2)
+                    Dim MSec = RECTIFIEDTIME.Split(".")(1)
+                    BData.Add("Year", Year)
+                    BData.Add("Month", Month)
+                    BData.Add("Day", Day)
+                    BData.Add("Hour", Hour)
+                    BData.Add("Min", Min)
+                    BData.Add("Sec", Sec)
+                    BData.Add("mSec", MSec)
+                    DateTimeB = BData
+                    Dim Place = ""
+                    Dim State = ""
+                    Dim Country = ""
+                    If RECTIFIEDPLACE.Contains("-") = True Then
+                        Place = RECTIFIEDPLACE.Split("-")(0)
+                        State = RECTIFIEDPLACE.Split("-")(1)
+                        Country = RECTIFIEDPLACE.Split("-")(2)
+                    Else
+                        Throw New System.Exception("Invalid RECTIFIEDPLACE format exception.")
+                    End If
+#Region "Personal Details"
+                    personalDetails.DateofBirth = RECTIFIEDDATE.Split(" ")(0)
+                    personalDetails.DayofBirth = GetDayOfTheWeek(DateTime.Parse(RECTIFIEDDATE.Split(" ")(0).Replace("-", "/")).DayOfWeek)
+                    personalDetails.TimeofBirth = RECTIFIEDDATE.Split(" ")(1)
+                    personalDetails.PlaceofBirth = RECTIFIEDPLACE
+                    personalDetails.Latitude = RECTIFIEDLATITUDE
+                    personalDetails.Longitude = RECTIFIEDLONGTITUDE
+                    connection.Open()
+                    Dim command As New SqlCommand($"select USERNAME from UPROF WHERE USERID = '" + UID + "';", connection)
+                    Dim da0 As New SqlDataAdapter(command)
+                    Dim ds As New DataSet()
+                    da0.Fill(ds)
+                    connection.Close()
+                    personalDetails.NameoftheChartOwner = ds.Tables(0).Rows(0)(0).Trim.ToString()
+                    personalDetails.Rashi = "Not yet provided by DLL"
+                    personalDetails.Star = "Not yet provided by DLL"
+                    personalDetails.SunSign = "Not yet provided by DLL"
+                    personalDetails.BalanceDasa = "Not yet provided by DLL"
+                    personalDetails.OriginalDOBByChartOwner = "Not yet provided by DLL"
+                    personalDetails.OriginalPOBByChartOwner = "Not yet provided by DLL"
+                    personalDetails.OriginalTOBByChartOwner = "Not yet provided by DLL"
+                    personalDetails.Marriage = "Not yet provided by DLL"
+                    personalDetails.FirstChild = "Not yet provided by DLL"
+                    personalDetails.LastCallRecieved = "Not yet provided by DLL"
+                    personalDetails.DemiseOfRelatives = "Not yet provided by DLL"
+#End Region
+                    Dim lon, lat, geoLat, Tz As Double
+                    lon = RECTIFIEDLONGTITUDE.Split("^")(0) + RECTIFIEDLONGTITUDE.Split("^")(1) / 60 + RECTIFIEDLONGTITUDE.Split("^")(2) / 3600
+                    geoLat = RECTIFIEDLATITUDE.Split("^")(0) + RECTIFIEDLATITUDE.Split("^")(1) / 60 + RECTIFIEDLATITUDE.Split("^")(2) / 3600
+                    Dim eW = RECTIFIEDLONGTITUDEEW
+                    Dim nS = RECTIFIEDLATITUDENS
+                    If eW = "W" Then
+                        lon = -lon
+                        Tz = -Tz
+                    End If
+                    lat = (Math.Atan(Math.Tan(geoLat * Math.PI / 180) * 0.99330546)) * 180 / Math.PI
+                    If nS = "S" Then
+                        lat = -lat
+                    End If
+                    Dim TzB = Val(RECTIFIEDTZ.Split(":")(0).Substring(1, 2)) + RECTIFIEDTZ.Split(":")(1) / 60
+                    Dim DstB = RECTIFIEDDST
+                    PlaceData.Add("Place", Place)
+                    PlaceData.Add("State", State)
+                    PlaceData.Add("Country", Country)
+                    PlaceData.Add("lonB", lon)
+                    PlaceData.Add("latB", lat)
+                    PlaceData.Add("eW", eW)
+                    PlaceData.Add("nS", nS)
+                    PlaceData.Add("TzB", TzB)
+                    PlaceData.Add("DstB", DstB)
+                    PlaceDataB = PlaceData
+                    Horo.getBirthPlanetCusp(DateTimeB, PlaceDataB, P_list, H_List)
+                    Horo.getBirthLagnaBhav(DateTimeB, PlaceDataB, BirthLagna, BirthBhav, BirthSouth)
+                    Dim DasaListP As New DataTable
+                    DasaListP.Columns.Add("Dasha")
+                    DasaListP.Columns.Add("Bhukti")
+                    DasaListP.Columns.Add("Antara")
+                    DasaListP.Columns.Add("Suk")
+                    DasaListP.Columns.Add("Pra")
+                    DasaListP.Columns.Add("Cl_Date")
+
+                    Horo.getBirthDasaFull(DateTimeB, PlaceDataB, DasaListP)
+                    Dim genChart As GenChart = New GenChart()
+                    genChart.UpdateHCUSP(HID, UID, H_List)
+                    genChart.UpdateHPLANET(HID, UID, P_list)
+                    genChart.UpdateHDASA(HID, UID, DasaListP)
+                    Dim updateCusp As UpdateCusp = New UpdateCusp()
+                    updateCusp.UpdateCusp(HID, UID)
+                    Dim genHTMLChart As GenHTMLChart = New GenHTMLChart()
+                    genHTMLChart.GenHTMLChartMain(HID, UID, P_list, H_List, BirthLagna, BirthBhav, BirthSouth, DasaListP, personalDetails)
+                    genChart.UpdateStatus(HID, UID)
+                Catch ex As Exception
+                    Dim strFile As String = String.Format("C:\inetpub\wwwroot\ServiceLogs\ErrorLog_{1}_{0}.txt", HID + UID, DateTime.Today.ToString("ddMMMyyyy"))
+                    File.AppendAllText(strFile, String.Format(vbCrLf + "Error Occured at-- {0}{1}{2}", Environment.NewLine + DateTime.Now, Environment.NewLine, ex.Message + vbCrLf + ex.StackTrace))
+                End Try
+            Next
+        Catch ex As Exception
+            Dim strFile As String = String.Format("C:\inetpub\wwwroot\ServiceLogs\ErrorLog_{1}_{0}.txt", HID + UID, DateTime.Today.ToString("ddMMMyyyy"))
+            File.AppendAllText(strFile, String.Format(vbCrLf + "Error Occured at-- {0}{1}{2}", Environment.NewLine + DateTime.Now, Environment.NewLine, ex.Message + vbCrLf + ex.StackTrace))
+        Finally
+            connection.Close()
+        End Try
+    End Sub
+    Public Shared Function GetDayOfTheWeek(ByVal day As String) As String
+        Dim DayOfWeek() = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}
+        Return DayOfWeek(Convert.ToInt32(day))
+    End Function
+    Sub UpdateHCUSP(ByRef HID As String, ByRef UID As String, ByRef H_List As String())
+        Dim con As New SqlConnection
+        Dim cmd As New SqlCommand
+        Try
+            con.ConnectionString = "data source=49.50.103.132;initial catalog=ASTROLOGYSOFTWARE_DB;integrated security=False;User Id=sa;password=pSI)TA1t0K[)"
+            con.Open()
+            cmd.Connection = con
+            Dim flag = False
+            For Each rowData In H_List
+                If flag = False Then
+                    flag = True
+                    Continue For
+                End If
+                cmd.CommandText = "INSERT INTO HEADLETTERS_ENGINE.DBO.HCUSP VALUES ('" + UID + "','" + HID + "','" + GetCuspNo(rowData.Split("|")(0)) + "','" + rowData.Split("|")(1) + "','" + rowData.Split("|")(2) + "','" + rowData.Split("|")(3) + "','" + rowData.Split("|")(4) + "','" + rowData.Split("|")(5) + "');"
+                cmd.ExecuteNonQuery()
+            Next
+        Catch ex As Exception
+            Dim strFile As String = String.Format("C:\inetpub\wwwroot\ServiceLogs\ErrorLog_{1}_{0}.txt", HID + UID, DateTime.Today.ToString("ddMMMyyyy"))
+            File.AppendAllText(strFile, String.Format(vbCrLf + "Error Occured at-- {0}{1}{2}", Environment.NewLine + DateTime.Now, Environment.NewLine, ex.Message + vbCrLf + ex.StackTrace))
+        Finally
+            con.Close()
+        End Try
+    End Sub
+    Sub UpdateHPLANET(ByRef HID As String, ByRef UID As String, ByRef P_List As String())
+        Dim con As New SqlConnection
+        Dim cmd As New SqlCommand
+        Try
+            con.ConnectionString = "data source=49.50.103.132;initial catalog=ASTROLOGYSOFTWARE_DB;integrated security=False;User Id=sa;password=pSI)TA1t0K[)"
+            con.Open()
+            cmd.Connection = con
+            Dim flag = False
+            For Each rowData In P_List
+                If flag = False Then
+                    flag = True
+                    Continue For
+                End If
+                cmd.CommandText = "INSERT INTO HEADLETTERS_ENGINE.DBO.HPLANET VALUES ('" + UID + "','" + HID + "','" + GetPlanetShortName(rowData.Split("|")(0)) + "','" + rowData.Split("|")(1) + "','" + rowData.Split("|")(2) + "','" + rowData.Split("|")(3) + "','" + rowData.Split("|")(4) + "','" + rowData.Split("|")(5) + "','" + rowData.Split("|")(6) + "','" + rowData.Split("|")(7) + "','" + rowData.Split("|")(8) + "','" + GetCuspPrefix(rowData.Split("|")(10)) + "');"
+                cmd.ExecuteNonQuery()
+            Next
+        Catch ex As Exception
+            Dim strFile As String = String.Format("C:\inetpub\wwwroot\ServiceLogs\ErrorLog_{1}_{0}.txt", HID + UID, DateTime.Today.ToString("ddMMMyyyy"))
+            File.AppendAllText(strFile, String.Format(vbCrLf + "Error Occured at-- {0}{1}{2}", Environment.NewLine + DateTime.Now, Environment.NewLine, ex.Message + vbCrLf + ex.StackTrace))
+        Finally
+            con.Close()
+        End Try
+    End Sub
+    Sub UpdateHDASA(ByRef HID As String, ByRef UID As String, ByRef DasaListP As DataTable)
+        Dim con As New SqlConnection
+        Dim cmd As New SqlCommand
+        Try
+            con.ConnectionString = "data source=49.50.103.132;initial catalog=ASTROLOGYSOFTWARE_DB;integrated security=False;User Id=sa;password=pSI)TA1t0K[)"
+            con.Open()
+            cmd.Connection = con
+            Dim flag = False
+            For i As Integer = 0 To DasaListP.Rows.Count - 1
+                cmd.CommandText = "INSERT INTO HEADLETTERS_ENGINE.DBO.HDASA VALUES ('" + UID + "','" + HID + "','" + ((DasaListP.Rows.Item(i)).ItemArray(0) + (DasaListP.Rows.Item(i)).ItemArray(1) + (DasaListP.Rows.Item(i)).ItemArray(2) + (DasaListP.Rows.Item(i)).ItemArray(3) + (DasaListP.Rows.Item(i)).ItemArray(4)).ToString().ToUpper() + "','" + Convert.ToString(DasaListP.Rows.Item(i).ItemArray(5)).Split(" :: ")(0) + "','" + Convert.ToString(DasaListP.Rows.Item(i).ItemArray(5)).Split(" :: ")(2) + "');"
+                cmd.ExecuteNonQuery()
+            Next
+        Catch ex As Exception
+            Dim strFile As String = String.Format("C:\inetpub\wwwroot\ServiceLogs\ErrorLog_{1}_{0}.txt", HID + UID, DateTime.Today.ToString("ddMMMyyyy"))
+            File.AppendAllText(strFile, String.Format(vbCrLf + "Error Occured at-- {0}{1}{2}", Environment.NewLine + DateTime.Now, Environment.NewLine, ex.Message + vbCrLf + ex.StackTrace))
+        Finally
+            con.Close()
+        End Try
+    End Sub
+    Sub UpdateStatus(ByRef HID As String, ByRef UID As String)
+        Dim con As New SqlConnection
+        Dim command As New SqlCommand
+        Try
+            con.ConnectionString = "data source=49.50.103.132;initial catalog=ASTROLOGYSOFTWARE_DB;integrated security=False;User Id=sa;password=pSI)TA1t0K[)"
+            con.Open()
+            command.Connection = con
+            command.CommandText = $"UPDATE HREQUEST
+                                    SET REQCAT = '7'
+                                    WHERE RQUSERID = '" + UID + "'
+                                        AND RQHID = '" + HID + "' 
+                                            AND HREQUEST.REQCAT = '9' 
+                                                AND HREQUEST.RQUNREAD = 'Y';"
+            command.ExecuteNonQuery()
+        Catch ex As Exception
+            Dim strFile As String = String.Format("C:\inetpub\wwwroot\ServiceLogs\ErrorLog_{1}_{0}.txt", HID + UID, DateTime.Today.ToString("ddMMMyyyy"))
+            File.AppendAllText(strFile, String.Format(vbCrLf + "Error Occured at-- {0}{1}{2}", Environment.NewLine + DateTime.Now, Environment.NewLine, ex.Message + vbCrLf + ex.StackTrace))
+        Finally
+            con.Close()
+        End Try
+    End Sub
+    Function GetCuspNo(ByVal s As String)
+        Dim ov = "NA"
+        Select Case s
+            Case "Cusp I"
+                ov = "01"
+            Case "Cusp II"
+                ov = "02"
+            Case "Cusp III"
+                ov = "03"
+            Case "Cusp IV"
+                ov = "04"
+            Case "Cusp V"
+                ov = "05"
+            Case "Cusp VI"
+                ov = "06"
+            Case "Cusp VII"
+                ov = "07"
+            Case "Cusp VIII"
+                ov = "08"
+            Case "Cusp IX"
+                ov = "09"
+            Case "Cusp X"
+                ov = "10"
+            Case "Cusp XI"
+                ov = "11"
+            Case "Cusp XII"
+                ov = "12"
+            Case Else
+        End Select
+        Return ov
+    End Function
+    Function GetPlanetShortName(ByVal Pl As String)
+        Dim Planet As String = "Not_A_Planet"
+        Select Case Pl
+            Case "Mars"
+                Planet = "MA"
+            Case "Venus"
+                Planet = "VE"
+            Case "Saturn"
+                Planet = "SA"
+            Case "Jupiter"
+                Planet = "JU"
+            Case "Sun"
+                Planet = "SU"
+            Case "Moon"
+                Planet = "MO"
+            Case "Mercury"
+                Planet = "ME"
+            Case "T.Rahu"
+                Planet = "RA"
+            Case "T.Ketu"
+                Planet = "KE"
+            Case "Neptune"
+                Planet = "NE"
+            Case "Pluto"
+                Planet = "PL"
+            Case "Uranus"
+                Planet = "UR"
+        End Select
+        Return Planet
+    End Function
+    Function GetCuspPrefix(ByVal s As String)
+        Dim ov = "NA"
+        Select Case s
+            Case "1"
+                ov = "01"
+            Case "2"
+                ov = "02"
+            Case "3"
+                ov = "03"
+            Case "4"
+                ov = "04"
+            Case "5"
+                ov = "05"
+            Case "6"
+                ov = "06"
+            Case "7"
+                ov = "07"
+            Case "8"
+                ov = "08"
+            Case "9"
+                ov = "09"
+            Case "10"
+                ov = "10"
+            Case "11"
+                ov = "11"
+            Case "12"
+                ov = "12"
+            Case Else
+        End Select
+        Return ov
+    End Function
+End Class
---
+++ Headletters_GenChart/ProjectInstaller.Designer.vb
@@ -0,0 +1,48 @@
+﻿<System.ComponentModel.RunInstaller(True)> Partial Class ProjectInstaller
+    Inherits System.Configuration.Install.Installer
+
+    'Installer overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Component Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Component Designer
+    'It can be modified using the Component Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.HeadlettersGenChartInstaller = New System.ServiceProcess.ServiceProcessInstaller()
+        Me.HeadlettersGenChartService = New System.ServiceProcess.ServiceInstaller()
+        '
+        'HeadlettersGenChartInstaller
+        '
+        Me.HeadlettersGenChartInstaller.Account = System.ServiceProcess.ServiceAccount.LocalSystem
+        Me.HeadlettersGenChartInstaller.Password = Nothing
+        Me.HeadlettersGenChartInstaller.Username = Nothing
+        '
+        'HeadlettersGenChartService
+        '
+        Me.HeadlettersGenChartService.Description = "Headletters Generate Chart triggers in every 1 minute"
+        Me.HeadlettersGenChartService.DisplayName = "Headletters_GenChart"
+        Me.HeadlettersGenChartService.ServiceName = "HeadlettersGenChartService"
+        Me.HeadlettersGenChartService.StartType = System.ServiceProcess.ServiceStartMode.Automatic
+        '
+        'ProjectInstaller
+        '
+        Me.Installers.AddRange(New System.Configuration.Install.Installer() {Me.HeadlettersGenChartInstaller, Me.HeadlettersGenChartService})
+
+    End Sub
+
+    Friend WithEvents HeadlettersGenChartInstaller As ServiceProcess.ServiceProcessInstaller
+    Friend WithEvents HeadlettersGenChartService As ServiceProcess.ServiceInstaller
+End Class
